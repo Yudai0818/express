d7ea99c9e92c5e727ba92553a56ca3ac47aa5dd6
比嘉侑大 <higayudai@hikayuudainoMacBook-Air.local>
2024-08-04 19:21:18 +0900

    add

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri ="**********";
+const uri ="mongodb+srv://Yudai0818:Y12588higa@test.ytgdh8c.mongodb.net/?retryWrites=true&w=majority&appName=test";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
